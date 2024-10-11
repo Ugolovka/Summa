758bcb3d57eb3a3137b17c1a74effbad100c5afa
Ugolovka <Medusa_Gangrena@tutamail.com>
2024-10-11 16:32:02 +0300

    ok4

--- summa.py
+++ summa.py
@@ -1,15 +1,15 @@
-input_data = open('input.txt','r') 
-data = input_data.read() 
+input_data = open("input.txt","r")
+data = input_data.read()
 a = int(data)
-b = 0
-if a>=0:
-    for i in range (1,a+1)
-    i+=b
-else
-    for i in range (0,2)
-    i+=b
-    k=str(i)
-output_data = open('output.txt', 'w')
-output_data.write(k)
+sum = 0
+if a >0:
+    for i in range(1,a + 1):
+        sum = sum + i
+else:
+    for i in range(a,2):
+        sum = sum + i
+b = str(sum)
+output_data = open("output.txt","w")
+output_data.write(b)
 input_data.close()
 output_data.close()
