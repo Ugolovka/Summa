42d23fd71fbe60cac0608bacbf045710e6864878
Ugolovka <Medusa_Gangrena@tutamail.com>
2024-10-11 16:12:34 +0300

    ok

---
+++ summa.py
@@ -0,0 +1,15 @@
+input_data = open('input.txt','r') 
+data = input_data.read() 
+a = int(data)
+b = 0
+if a>0:
+    for i in range (1,a+1)
+    i+=b
+    else
+    for i in range (0,2)
+    i+=b
+    k=str(i)
+output_data = open('output.txt', 'w')
+output_data.write(k)
+input_data.close()
+output_data.close()
